192a3c3924dc28d22743d23275fcab1fb9807b0e
Leonardo de Abreu <leonardo.abreu@lia.ufc.br>
2023-05-31 20:59:22 -0300

    Desfaz ultima mudança

--- files/add-epson-scanner.sh
+++
@@ -1,4 +0,0 @@
-(test -f $HOME/.epsonscan2/Network/epsonscan2.conf &&
-	grep -q "10.102.54.103" $HOME/.epsonscan2/Network/epsonscan2.conf )||
-	( test -f /usr/bin/epsonscan2 &&
-		/usr/bin/epsonscan2 --set-ip 10.102.54.103 > /dev/null )
